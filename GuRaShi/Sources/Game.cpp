dcda9a98342d4afecaec4c487225b43d8de6a29d
hw16a118 <hw16a118@oecu.jp>
2017-12-05 18:16:20 +0900

    D_E_Fを実装

--- GuRaShi/Sources/Game.cpp
+++ GuRaShi/Sources/Game.cpp
@@ -4,9 +4,9 @@
 // TODO: 砲台の位置を画面左に、ターゲットの位置を画面右に移動させる。(A)
 // TODO: 雲の位置を左から右に動かす。見えなくなったら左端に戻す。(B)
 // TODO: 砲台を青い壁に沿って上下に動かす。(C)
-// TODO: 弾のスピードを速くし、弾が画面右端を通り越したら再度発射可能にする。(D)
-// TODO: スコアのサイズを大きくする。(E)
-// TODO: スコアを100点ずつ加算するようにし、5桁の表示に変える。(F)
+// TODO: 弾のスピードを速くし、弾が画面右端を通り越したら再度発射可能にする。(D)　hw16a118_谷口遼樹
+// TODO: スコアのサイズを大きくする。(E) HW16A118_谷口遼樹
+// TODO: スコアを100点ずつ加算するようにし、5桁の表示に変える。(F) HW16A118_谷口遼樹
 // TODO: PlayBGM()関数を使って、BGMを再生する。(G)
 // TODO: PlaySE()関数を使って、弾の発射時とターゲットに当たった時にSEを再生する。(H)
 
@@ -16,44 +16,53 @@ Vector2 cannonPos;      //!< 砲台の位置
 Vector2 bulletPos;      //!< 弾の位置
 Rect    targetRect;     //!< ターゲットの矩形
 int     score;          //!< スコア
-
+int     flag;
 
 // ゲーム開始時に呼ばれる関数です。
 void Start()
 {
     cloudPos = Vector2(-320, 100);
-    cannonPos = Vector2(-80, -150);
-    targetRect = Rect(80, -140, 40, 40);
+    cannonPos = Vector2(-310, -150);
+    targetRect = Rect(280, -140, 40, 40);
     bulletPos.x = 700;
-    score = 0;
+    score = 000000;
+    //>>>>>>> 695d404c0530aa1b6cd77c937f3ab31e67db8a2d
+    bulletPos.x = 700;
+    score = 000000;
+    PlayBGM("bgm_maoudamashii_8bit07.mp3");
 }
 
+
 // 1/60秒ごとに呼ばれる関数です。モデルの更新と画面の描画を行います。
-void Update()
 {
     // 弾の発射
-    if (bulletPos.x >= 300 && Input::GetKeyDown(KeyMask::Space)) {
+    if (bulletPos.x >= 320   && Input::GetKeyDown(KeyMask::Space)) {
         bulletPos = cannonPos + Vector2(50, 10);
+        PlaySound("se_maoudamashii_system20.mp3");
     }
-
+    
     // 弾の移動
     if (bulletPos.x > -999) {
         bulletPos.x += 300 * Time::deltaTime;
-
+        bulletPos.x += 100 * Time::deltaTime;
+        
         // ターゲットと弾の当たり判定
         Rect bulletRect(bulletPos, Vector2(32, 20));
         if (targetRect.Overlaps(bulletRect)) {
-            score += 1;         // スコアの加算
+            score += 100;         // スコアの加算
             bulletPos.x = 700; // 弾を発射可能な状態に戻す
+            PlaySound("se_maoudamashii_explosion06.mp3");
         }
     }
-
     // 背景の描画
     Clear(Color::cyan);
     FillRect(Rect(-320, -240, 640, 100), Color::green);
 
     // 雲の描画
     DrawImage("cloud1.png", cloudPos);
+    if (cloudPos.x > -999) {
+        cloudPos.x += 200 * Time::deltaTime;
+    }
 
     // 弾の描画
     if (bulletPos.x < 700) {
@@ -68,8 +77,8 @@ void Update()
     FillRect(targetRect, Color::red);
 
     // スコアの描画
-    SetFont("nicoca_v1.ttf", 20.0f);
-    DrawText(FormatString("%02d", score), Vector2(-319, 199), Color::black);
-    DrawText(FormatString("%02d", score), Vector2(-320, 200), Color::white);
+    SetFont("nicoca_v1.ttf", 50.0f);
+    DrawText(FormatString("%05d", score), Vector2(-319, 199), Color::black);
+    DrawText(FormatString("%05d", score), Vector2(-320, 200), Color::white);
 }
 
